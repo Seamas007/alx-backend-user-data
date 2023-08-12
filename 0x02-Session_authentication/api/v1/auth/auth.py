4daa8e0c57aa95f37ad7adaa70e07fefde61946c
Seamas007 <olufy007@gmail.com>
2023-08-12 15:22:32 +0100

    My commit

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,46 +1,61 @@
 #!/usr/bin/env python3
-"""Authentication module for the API.
 """
-import os
-import re
-from typing import List, TypeVar
+Module of auth class
+"""
 from flask import request
+from typing import List, TypeVar
+import fnmatch
 
 
-class Auth:
-    """Authentication class.
+class Auth(object):
+    """Template for all authentication system
+    implemented in the project
     """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication.
+
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool:
         """
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    return False
+
+        Args:
+          - path(string): path to return
+          - excluded_paths(list): list of paths to exclude
+
+        Returns:
+          - True if is authenticated otherwise false
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Gets the authorization header field from the request.
         """
-        if request is not None:
-            return request.headers.get('Authorization', None)
-        return None
+
+        Args:
+          - request(Flask request): request object to make
+
+        Returns:
+          - The string: 'None - request'
+        """
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Gets the current user from the request.
+        """
+
+        Args:
+          - request(request object): request object to make
+
+        Returns:
+          - Any type
         """
         return None
-
-    def session_cookie(self, request=None) -> str:
-        """Gets the value of the cookie named SESSION_NAME.
-        """
-        if request is not None:
-            cookie_name = os.getenv('SESSION_NAME')
-            return request.cookies.get(cookie_name)
